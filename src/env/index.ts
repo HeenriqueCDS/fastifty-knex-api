8fc2cdbd13e40fd183a1ab6dc98b6db0ec749f34
HeenriqueCDS <heenriquecds@gmail.com>
2023-07-30 11:16:53 -0300

    fix app instance for testing

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z
-    .enum(['development', 'staging', 'production'])
+    .enum(['development', 'staging', 'production', 'test'])
     .default('development'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
