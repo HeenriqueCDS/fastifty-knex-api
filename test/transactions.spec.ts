3ce49dc696e225dffacb70e2d24da0cdf817b26e
HeenriqueCDS <heenriquecds@gmail.com>
2023-07-30 11:52:26 -0300

    test database

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,7 +1,15 @@
-import { it, beforeAll, afterAll, describe, expect } from 'vitest'
+import {
+  it,
+  beforeAll,
+  afterAll,
+  describe,
+  expect,
+  beforeEach,
+  afterEach,
+} from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
-
 describe('Transactions Routes', () => {
   beforeAll(async () => {
     await app.ready()
@@ -9,6 +17,14 @@ describe('Transactions Routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:latest')
+  })
+
+  afterEach(() => {
+    execSync('npm run knex migrate:rollback --all')
   })
 
   it('should be able to create a new transaction', async () => {
